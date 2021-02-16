ae8617266dcaa741ae7ad0378228721a70aaaa23
palchandu <chandu2013pal@gmail.com>
2021-02-16 13:33:52 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -1,21 +1,13 @@
-//var express=require('express');
 import express from 'express';
 var app=express();
-//var bodyparser=require('body-parser');
 import bodyparser from 'body-parser';
-/*Import Routes */
-//var user=require('./routes/user.route');
 import 'dotenv/config';
+/*Import Routes */
 import user from './routes/user.route'
 
 app.use(bodyparser.urlencoded({ limit: '50mb',extended: true }));
 app.use(bodyparser.json({limit: '50mb',extended: true}));
 
-
-/*test routes */
-// app.use('/',function(req,res){
-//     console.log("Hello World!");
-// })
 /*Use Routes */
 app.use('/',user);
 
---
+++ src/routes/indian_railway.js
@@ -0,0 +1,5 @@
+import express from 'express';
+var railway_router=express.Router();
+
+
+module.exports=railway_router;
